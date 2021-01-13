8dd29b68d22078f4f7b1f8c959bb0370b5585f20
Tom van Enckevort <tom@happyporch.com>
2021-01-13 10:04:33 +0000

    Initial parse test.

--- app/controllers/discourse-media-wiki-import/import_controller.rb
+++ app/controllers/discourse-media-wiki-import/import_controller.rb
@@ -4,7 +4,18 @@ module DiscourseMediaWikiImport
     def import
       params.require([:uploadedMediaWikiExportUrl, :categoryId])
 
-      render_json_dump(params[:uploadedMediaWikiExportUrl])
+      upload = Upload.get_from_url(params[:uploadedMediaWikiExportUrl])
+      local_path = Discourse.store.path_for(upload)
+
+      processed_pages = 0
+
+      mwns = "http://www.mediawiki.org/xml/export-0.10/"
+
+      Nokogiri::XML(File.open(local_path)).xpath("//mediawiki:page", "mediawiki"=>mwns).each do |page|
+        processed_pages += 1
+      end
+
+      render_json_dump("Successfully imported #{processed_pages} MediaWiki articles")
     end
 
   end
